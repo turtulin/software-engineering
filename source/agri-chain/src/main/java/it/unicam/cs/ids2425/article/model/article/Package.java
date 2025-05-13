9300006c3d0f43ee82fbea6ae9feaf9607076c54
feedz <feedesigner@gmail.com>
2025-05-13 12:25:19 +0200

    [fix] Package components and ProcessedProduct ingredients

--- source/agri-chain/src/main/java/it/unicam/cs/ids2425/article/model/article/Package.java
+++ source/agri-chain/src/main/java/it/unicam/cs/ids2425/article/model/article/Package.java
@@ -5,20 +5,20 @@ import it.unicam.cs.ids2425.article.model.ArticleType;
 import it.unicam.cs.ids2425.article.model.article.compositearticle.ComposableArticle;
 import it.unicam.cs.ids2425.user.model.User;
 import jakarta.persistence.Entity;
-import jakarta.persistence.OneToMany;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ManyToMany;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Getter
+@EqualsAndHashCode(callSuper = true)
+@Data
 @Entity
 @ToString(callSuper = true)
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class Package extends Article {
-    @OneToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     private List<ComposableArticle> components;
 
     public Package(ArticleType type, String name, String description, double price, User seller) {
--- source/agri-chain/src/main/java/it/unicam/cs/ids2425/article/model/article/compositearticle/ProcessedProduct.java
+++ source/agri-chain/src/main/java/it/unicam/cs/ids2425/article/model/article/compositearticle/ProcessedProduct.java
@@ -2,16 +2,24 @@ package it.unicam.cs.ids2425.article.model.article.compositearticle;
 
 import it.unicam.cs.ids2425.article.model.ArticleType;
 import it.unicam.cs.ids2425.user.model.User;
-import jakarta.persistence.Entity;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
 @Entity
 @ToString(callSuper = true)
 @NoArgsConstructor(access = lombok.AccessLevel.PROTECTED)
 public class ProcessedProduct extends ComposableArticle {
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<ComposableArticle> ingredients;
+
     public ProcessedProduct(ArticleType type, String name, String description, double price, User seller) {
         super(type, name, description, price, seller);
+        this.ingredients = new ArrayList<>();
         if (type != ArticleType.PROCESSED_PRODUCT) {
             throw new IllegalArgumentException("The type of a raw material must be PROCESSED_PRODUCT");
         }
